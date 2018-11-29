ae1865ef5d6c820bb81ca94f3d926f6a1b6dbf15
AlperROzkan <alper.ozkan@etu.univ-nantes.fr>
2018-11-29 19:07:16 +0100

    Ajout de la musique

--- SpaceLander.py
+++ SpaceLander.py
@@ -19,6 +19,9 @@ pygame.display.set_caption("Space Lander")
 # Chargement image
 imgBalloon = pygame.image.load("Balloon.png")
 
+# Musique
+music1 = pygame.mixer_music.load("brodyquest1.mp3")
+
 # Verifie si il y a un game over
 # murs : Classe Wall : les murs de la partie
 # fusee : Classe Fusee : la fusee
@@ -80,6 +83,8 @@ def gagne(murs, fusee, tolerance) :
 
 # Fonction definissant le jeu
 def gameLoop():
+    # Musique
+    pygame.mixer_music.play(1,0.0)
     # Horloge
     clock = pygame.time.Clock()
     font = pygame.font.Font('spacelander.ttf',30)
