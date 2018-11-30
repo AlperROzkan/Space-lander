4d17e2a94231d0992f3390bf3409d9080dbce95f
AlperROzkan <alper.ozkan@etu.univ-nantes.fr>
2018-11-30 14:27:22 +0100

    Fix de la derniere plateforme non coloriee

--- Wall.py
+++ Wall.py
@@ -3,16 +3,24 @@ import pygame
 from random import *
 
 white = (255,255,255)
+blue = (0, 100, 175)
 
 
 # Classe qui décrit un mur a laide de pygame.draw.lines
 class Wall:
     # Constructeur de la classe
     # window : ecran de jeu
+    # liste_sols : les sols en tant que rectangles
+    # liste_points : les points dans une liste
+    # liste_atterissage : les deuxiemes points des pistes d'atterissages
+    #
     def __init__(self, window):
         self.window = window
         self.liste_sols = []
-        self.liste_points = 0
+        self.liste_points = []
+        self.liste_atterissage = []
+        self.largeur_atterissage = 90
+        self.nb_plateforme = 0
 
     # Mehode generant aléatoirement les points a lier a partir des caractérisriques de window que l'on donne au
     # constructeur
@@ -36,8 +44,10 @@ class Wall:
             # Generation terrain plat
             proba_terrain_plat = randint(0,100)
             if proba_terrain_plat < 10 :
-                largeur_restante=largeur_restante+90
+                largeur_restante=largeur_restante+self.largeur_atterissage
                 self.liste_points.append([largeur_restante,hauteur_mur])
+                self.liste_atterissage.append([largeur_restante,hauteur_mur])
+
         # Retour de la liste
         return self.liste_points
 
@@ -47,11 +57,10 @@ class Wall:
     def draw_wall(self, liste_points):
         for i in range(0, len(liste_points) - 1):
             self.liste_sols.append(pygame.draw.line(self.window, white, (liste_points[i][0], liste_points[i][1]), (liste_points[i+1][0], liste_points[i+1][1]),1))
-            #pygame.draw.rect(self.window, (255,0,0), self.liste_sols[i], 1)
+
+        for j in range(0, len(self.liste_atterissage)) :
+            pygame.draw.line(self.window, blue, (self.liste_atterissage[j][0]-self.largeur_atterissage, self.liste_atterissage[j][1]), (self.liste_atterissage[j][0], self.liste_atterissage[j][1]), 3)
 
     # Methode permettant d'avoir la liste des points
     def donne_points(self):
         return self.liste_points
-
-
-
